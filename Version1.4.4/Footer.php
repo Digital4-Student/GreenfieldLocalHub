f7effcb8419703bfe83fa895c6cc04886fee6d3c
Digital4-Student <digital4@student.loucoll.ac.uk>
2026-03-25 12:16:15 +0000

    Add files via upload
    Working on settings page

---
+++ Version1.4.4/Footer.php
@@ -0,0 +1,48 @@
+
+
+<nav class="navbar navbar-expand-sm NAVBAR-bg  py-4">
+
+    <div class="nav position-absolute top-50 start-50 translate-middle">
+        <ul class="navbar-nav " >
+            <li class="nav-item">
+            <a class="nav-link NAVLINK-colour" href="Privacy_policy.php">Privacy Policy</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link NAVLINK-colour" href="Cookie_policy.php">Cookie Policy</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link NAVLINK-colour" href="Terms_conditions.php">Terms & Conditions</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link NAVLINK-colour" href="Contact_Us.php">Contact us</a>
+            </li>    
+        </ul>
+    </div>
+
+
+    <div class="d-flex align-items-center gap-3 ms-auto">
+
+            <!--Facebook-->
+            
+            <div>
+                <a href="#">
+                <i class="fa-brands fa-square-facebook fa-2xl" style="color: rgb(255, 255, 255);"></i>
+                </a>
+            </div>
+
+            <!--Twitter-->
+            <div>
+                <a href="#">
+                <i class="fa-brands fa-square-twitter fa-2xl" style="color: rgb(255, 255, 255);"></i>
+                </a>
+            </div>
+
+            <!--Instagram-->
+            <div>
+                <a href="#">
+                <i class="fa-brands fa-square-instagram fa-2xl" style="color: rgb(255, 255, 255);"></i>
+                </a>
+            </div>
+    </div>
+
+</nav>
