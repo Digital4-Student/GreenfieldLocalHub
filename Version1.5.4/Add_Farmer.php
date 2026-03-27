df2dbc61e1a71b067e92adb5dc982eeadc6237f2
Digital4-Student <digital4@student.loucoll.ac.uk>
2026-03-27 10:49:24 +0000

    Add files via upload
    Added a farmer profile creation, begun on the product creation

---
+++ Version1.5.4/Add_Farmer.php
@@ -0,0 +1,36 @@
+<body class="d-flex flex-column min-vh-100 ">
+<?php include 'Navbar.php'; 
+
+if (!isset($_SESSION["role"]) || $_SESSION["role"] !== 'farmer') {
+    header("Location: Homepage.php");
+    exit;
+}
+?>
+
+<main class="flex-grow-1 pt-5 pb-5 body" >
+
+
+
+<div class="container signbox">
+    <br>
+    <form action="Add_Farmer_PHP.php" method="post">
+    <div class="mb-3 mt-3">
+        <label for="FaName" class="form-label">Farmer/Producer Name:</label>
+        <input type="text" class="form-control" id="FaName" Value="<?php echo $_SESSION['customer']['FirstName'] ?> <?php echo $_SESSION['customer']['LastName'] ?>" name="FaName">
+    </div>    
+    <div class="mb-3">
+        <label for="FDesc" class="form-label">Description:</label>
+        <textarea type="text" class="form-control" id="FDesc" placeholder="Enter description" name="FDesc"></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-success  ">Add profile</button>
+    <br><br>
+    </form>
+</div>
+
+</main>
+
+
+<?php include 'Footer.php'; ?>
+
+</body>
